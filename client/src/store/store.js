3bbb8a1222dfad927061fa02b491bf652160130c
Nitesh Kumar <122961768+nitesh098suthar@users.noreply.github.com>
2023-09-12 02:49:54 +0530

    Update store.js

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,14 +1,13 @@
-import {configureStore} from "@reduxjs/toolkit"
+import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./reducer/authReducer";
 import taskReducer from "./reducer/taskReducer";
 
-
-export const server_uri = 'http://localhost:9000'
+export const server_uri = "https://trello-clone-d83k.vercel.app";
 const store = configureStore({
-    reducer:{
-        taskReducer,
-        authReducer
-    }
-})
+  reducer: {
+    taskReducer,
+    authReducer,
+  },
+});
 
 export default store;
