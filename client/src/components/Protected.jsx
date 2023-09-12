aacd6527f1ac3ea3623e2c444f58bd58b3928a82
Nitesh Kumar <122961768+nitesh098suthar@users.noreply.github.com>
2023-09-12 15:50:18 +0530

    Update Protected.jsx

--- client/src/components/Protected.jsx
+++ client/src/components/Protected.jsx
@@ -1,12 +1,7 @@
-import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-
-const Protected = ({ children, isAuth }) => {
-  const nav = useNavigate();
-  useEffect(() => {
-    if (!isAuth) return nav("/register");
-  }, [isAuth, nav]);
+import { Navigate } from "react-router-dom";
+export const Protected = ({ isAuth, children }) => {
+  if (!isAuth) {
+    return <Navigate to="/" replace />;
+  }
   return children;
 };
-
-export default Protected;
